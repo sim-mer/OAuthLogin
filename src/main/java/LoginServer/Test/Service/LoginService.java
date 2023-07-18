07d44e13766d9d8a61faecb991a70e0d1a415ef7
Gyumin Sim <gyu2781@gmail.com>
2023-07-18 16:10:24 +0900

    [Feat] 서비스 레이어 - login or member 생성 기능 작성

---
+++ src/main/java/LoginServer/Test/Service/LoginService.java
@@ -0,0 +1,49 @@
+package LoginServer.Test.Service;
+
+import LoginServer.Test.DTO.Token;
+import LoginServer.Test.Domain.Member;
+import LoginServer.Test.Login.Info.OAuthInfo;
+import LoginServer.Test.Login.OAuthService;
+import LoginServer.Test.Login.Params.OAuthLoginParams;
+import LoginServer.Test.Repository.MemberRepository;
+import LoginServer.Test.Utils.Jwt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+
+
+
+@Service
+public class LoginService {
+    @Autowired
+    OAuthService oAuthService;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    Jwt jwt;
+
+    public Token kakao(OAuthLoginParams params) {
+        OAuthInfo oAuthInfo = oAuthService.request(params);
+        Long memberId = findOrCreateMember(oAuthInfo);
+        Token token = new Token();
+        token.setAccessToken(jwt.getAccessToken(memberId.toString(), null));
+        token.setRefreshToken(jwt.getRefreshToken(memberId.toString()));
+        return token;
+    }
+
+    private Long findOrCreateMember(OAuthInfo oAuthInfo) {
+        return memberRepository.findByEmail(oAuthInfo.getEmail())
+                .map(Member::getId)
+                .orElseGet(() -> newMember(oAuthInfo));
+    }
+
+    private Long newMember(OAuthInfo oAuthInfo) {
+        Member member = Member.builder()
+                .email(oAuthInfo.getEmail())
+                .oAuthProvider(oAuthInfo.getOAuthProvider())
+                .build();
+
+        return memberRepository.save(member).getId();
+    }
+}
