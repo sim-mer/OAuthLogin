9e235c8ea0dd300a2b76c3c43e903dc9a9b942e4
Gyumin Sim <gyu2781@gmail.com>
2023-07-18 16:10:42 +0900

    [Feat] MemberRepository 작성

---
+++ src/main/java/LoginServer/Test/Repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package LoginServer.Test.Repository;
+
+import LoginServer.Test.Domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+}
